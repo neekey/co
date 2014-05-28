f1539e1dab4b19c1328962d4eca422364434976c
Neekey <ni184775761@gmail.com>
2014-05-28 14:43:41 +0800

    完善注释

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function co(fn) {
     // #92
     // wrap the callback in a setImmediate
     // so that any of its errors aren't caught by `co`
+    // 让回掉方法异步执行，防止其中的错误co本身catch住
     function exit(err, res) {
       setImmediate(done.bind(ctx, err, res));
     }
@@ -70,6 +71,9 @@ function co(fn) {
       // error
       if (err) {
         try {
+          /**
+           * 感觉generator的throw和throw new Error 差不多，本身不会对ret进行赋值 todo 确认下throw的特性
+           */
           ret = gen.throw(err);
         } catch (e) {
           return exit(e);
@@ -79,6 +83,7 @@ function co(fn) {
       // ok
       if (!err) {
         try {
+          // 开始执行迭代...，并得到结果
           ret = gen.next(res);
         } catch (e) {
           return exit(e);
@@ -86,15 +91,25 @@ function co(fn) {
       }
 
       // done
+      // 若迭代器已经结束，则结束，并返回结果
       if (ret.done) return exit(null, ret.value);
 
       // normalize
+      // 若尚未结束，检查返回值，继续执行
+      // 用户在yield时可能会返回很多中类型的值（promise, generator...）
+      // 这里统一处理成一个 一个function，且这个function接收一个done参数，具体过程参考 toThunk方法的定义
       ret.value = toThunk(ret.value, ctx);
 
       // run
+      // 以下是co结合generator能实现串行且传值的关键
+      // 在generatorFunction中，通过使用 yield 将yield右边的值返回给ret.value
+      // 而在下一次的 gen.next() 中可以通过给 next() 方法传入第一个参数来为作为 yield 的返回值
+      // 从而达到 yield 中断前和中断后的传值的功能
       if ('function' == typeof ret.value) {
         var called = false;
         try {
+          
+          // 执行 ret.value，并将返回值传递给 next，进而传递给下一次的迭代器调用
           ret.value.call(ctx, function(){
             if (called) return;
             called = true;
@@ -118,6 +133,7 @@ function co(fn) {
 
 /**
  * Convert `obj` into a normalized thunk.
+ * 将object 转为一个接受一个done作为参数的方法
  *
  * @param {Mixed} obj
  * @param {Mixed} ctx
@@ -127,22 +143,28 @@ function co(fn) {
 
 function toThunk(obj, ctx) {
 
+  // 若为generatorFunction。
+  // 注意，从代码可以看到，如果yield返回了一个generatorFunction，它在后续执行返回generator的过程中只会被给到一个参数（回掉）
   if (isGeneratorFunction(obj)) {
     return co(obj.call(ctx));
   }
 
+  // 若为generator
   if (isGenerator(obj)) {
     return co(obj);
   }
 
+  // 若为promise
   if (isPromise(obj)) {
     return promiseToThunk(obj);
   }
 
+  // 若单纯为一个function，这个function会被简单地给定一个回掉函数作为第一个参数进行调用
   if ('function' == typeof obj) {
     return obj;
   }
 
+  // 若为数组或者对象
   if (isObject(obj) || Array.isArray(obj)) {
     return objectToThunk.call(ctx, obj);
   }
@@ -152,6 +174,9 @@ function toThunk(obj, ctx) {
 
 /**
  * Convert an object of yieldables to a thunk.
+ * 若为一个数组或者对象，会返回一个接受一个回掉函数作为第一个参数的函数。
+ * 该函数在执行过程中，会将所有的数组或者对象的function成员进行执行，搜集结果，如果这些成员不是funciton，则直接作为结果
+ * 最终所有的结果会统一放到一个对象中传递到 这个函数给定的回掉函数中
  *
  * @param {Object} obj
  * @return {Function}
